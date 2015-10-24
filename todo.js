714e08ea7757c30a44ef0cb62171b7586e9a77e3
mthirunavukkar <mthirunavukkar@vmware.com>
2015-10-24 15:17:26 -0700

    Added service

--- todo.js
+++ todo.js
@@ -1,9 +1,28 @@
 angular.module('todoApp', [])
-    .controller('TodoController', ['$scope', function($scope) {
-      $scope.todo = "";
-        $scope.todos = ["eat well", "have a beer"];
-        $scope.addTodo = function () {
-          $scope.todos.push($scope.todo);
-          $scope.todo = "";
-        };
-    }]);
+   .controller('TodoController', ['$scope', 'TodoService',
+      function($scope, TodoService) {
+         $scope.todo = "";
+         $scope.todos = TodoService.getTodos();
+         $scope.addTodo = function () {
+            TodoService.addTodo($scope.todo);
+            $scope.todo = "";
+         };
+      }])
+
+   .service('TodoService', [function(){
+      var todos = ["Netflix and chill"];
+      return {
+         getTodos: getTodos,
+         addTodo : addTodo
+      }
+
+      function getTodos() {
+         return todos;
+      }
+
+      function addTodo(todo) {
+         if (todo !== "") {
+            todos.push(todo);
+         }
+      }
+   }]);
